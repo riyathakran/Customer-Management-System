fd07280a0b7269253db7a3f35ec22c36e68341bd
Riya <riyathakran996@gmail.com>
2023-12-04 16:13:13 +0530

    initial commit

---
+++ SpringAngular/SpringAngular/src/main/java/com/Project/SpringAngular/Service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.Project.SpringAngular.Service;
+
+import com.Project.SpringAngular.DTO.CustomerDTO;
+import com.Project.SpringAngular.DTO.CustomerSaveDTO;
+import com.Project.SpringAngular.DTO.CustomerUpdateDTO;
+
+import java.util.List;
+
+public interface CustomerService {
+    String addCustomer(CustomerSaveDTO customerSaveDTO);
+
+    List<CustomerDTO> getAllCustomer();
+
+    String updateCustomer(CustomerUpdateDTO customerUpdateDTO);
+
+    Boolean deleteCustomer(int id);
+}
